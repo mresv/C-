3b458b1bcf335b43c6e3355bf5c4863c1110075b
Sergey Evseev <mresv@ya.ru>
2023-03-28 14:29:05 +0300

    добавлены случайные числа

--- Exp003_Sum/Program.cs
+++ Exp003_Sum/Program.cs
@@ -1,4 +1,7 @@
-﻿int numA = 3;
-int numB = 5;
+﻿int numA = new Random().Next(1,100);
+Console.WriteLine(numA);
+int numB = new Random().Next(1,100);
+Console.WriteLine(numB);
 int sum = numA+numB;
+Console.Write("Сумма чисел: ");
 Console.WriteLine(sum);
