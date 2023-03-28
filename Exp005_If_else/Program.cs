b2939154e0810e4c38761224c4d0a5e8136d4e1b
Sergey Evseev <mresv@ya.ru>
2023-03-28 14:53:21 +0300

    задача с узнаванием Маши

---
+++ Exp005_If_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура! Это же Маша!");
+}
+else
+{
+Console.Write("Приветствую тебя, ");
+Console.Write(username);
+}
